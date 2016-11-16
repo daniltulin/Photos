d859736564f43017f8252bfcda12988a9ba08647
tulindanil <tulindanil@gmail.com>
2016-11-16 16:30:52 +0300

    Initial Commit

---
+++ Photos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Photos
+//
+//  Created by Danil Tulin on 11/16/16.
+//  Copyright © 2016 Daniil Tulin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Photos/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Photos
+//
+//  Created by Danil Tulin on 11/16/16.
+//  Copyright © 2016 Daniil Tulin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
