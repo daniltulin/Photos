096b3ee282da12e3d019b38a8a0dd4ffb90dd654
tulindanil <tulindanil@gmail.com>
2016-11-17 12:04:15 +0300

    Initial commit

---
+++ Photos/Album.m
@@ -0,0 +1,13 @@
+//
+//  Album.m
+//  Photos
+//
+//  Created by Danil Tulin on 11/17/16.
+//  Copyright © 2016 Daniil Tulin. All rights reserved.
+//
+
+#import "Album.h"
+
+@implementation Album
+
+@end
