a9f2edfdab4a6a70d4f6884a4cfe6778f99d85f3
tulindanil <tulindanil@gmail.com>
2016-11-19 16:35:40 +0300

    Initial commit

---
+++ Photos/ImageManager.h
@@ -0,0 +1,13 @@
+//
+//  ImageManager.h
+//  Photos
+//
+//  Created by Danil Tulin on 11/19/16.
+//  Copyright © 2016 Daniil Tulin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ImageManager : NSObject
+
+@end
---
+++ Photos/ImageManager.m
@@ -0,0 +1,13 @@
+//
+//  ImageManager.m
+//  Photos
+//
+//  Created by Danil Tulin on 11/19/16.
+//  Copyright © 2016 Daniil Tulin. All rights reserved.
+//
+
+#import "ImageManager.h"
+
+@implementation ImageManager
+
+@end
