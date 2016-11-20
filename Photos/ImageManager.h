43d19f33b52de2493b54b5d922d231d458c8a6e1
tulindanil <tulindanil@gmail.com>
2016-11-20 17:46:05 +0300

    Implement ImageManager, remove caching options

--- Photos/ImageManager.h
+++ Photos/ImageManager.h
@@ -13,15 +13,12 @@ typedef void (^ImageResultHandler)(UIImage *image);
 @interface ImageManager : NSObject
 
 + (instancetype)managerWithAssets:(NSArray *)assets
-                     andImageSize:(CGSize)imageSize;
-
-- (void)startCaching;
-- (void)stopCaching;
+                     andImageSize:(CGSize)targetSize;
 
 - (void)fetchImageAtIndex:(NSInteger)index
               withHandler:(ImageResultHandler)handler;
 
-// In case if you need smaller size image than you specified
+// In case if you need bigger size image than you specified
 // in constructor
 - (void)fetchImageAtIndex:(NSInteger)index
            withTargetSize:(CGSize)targetSize
--- Photos/ImageManager.m
+++ Photos/ImageManager.m
@@ -10,42 +10,110 @@
 
 @interface ImageManager ()
 
+@property (nonatomic) PHImageManager *manager;
+
 @property (nonatomic) NSArray *assets;
-@property (nonatomic) CGSize imageSize;
+@property (nonatomic) CGSize targetSize;
+
+@property (nonatomic) PHImageContentMode contentMode;
+@property (nonatomic) PHImageRequestOptions *options;
+
+@property (nonatomic) NSMutableDictionary *requests;
 
 @end
 
 @implementation ImageManager
 
 + (instancetype)managerWithAssets:(NSArray *)assets
-                     andImageSize:(CGSize)imageSize {
+                     andImageSize:(CGSize)targetSize {
     ImageManager *manager = [[ImageManager alloc] init];
     manager.assets = assets;
-    manager.imageSize = imageSize;
+    manager.targetSize = targetSize;
     return manager;
-}
-
-- (void)startCaching {
-    
-}
-
-- (void)stopCaching {
-    
 }
 
 - (void)fetchImageAtIndex:(NSInteger)index
               withHandler:(ImageResultHandler)handler {
-    
+    [self fetchImageAtIndex:index
+             withTargetSize:self.targetSize
+                 andHandler:handler];
 }
 
 - (void)fetchImageAtIndex:(NSInteger)index
            withTargetSize:(CGSize)targetSize
                andHandler:(ImageResultHandler)handler {
+    NSNumber *identifier = [NSNumber numberWithInteger:index];
+    if (self.requests[identifier])
+        return;
     
+    PHAsset *asset = self.assets[index];
+    ResultHandler resultHandler = ^(UIImage *result,
+                                    NSDictionary *info){
+        [self.requests removeObjectForKey:identifier];
+        if ([info[PHImageCancelledKey] boolValue])
+            return;
+        if (info[PHImageErrorKey]) {
+            NSLog(@"Error was occured: %@", info[PHImageErrorKey]);
+            return;
+        }
+        handler(result);
+    };
+    ResultHandler resultHandlerWrapper = ^(UIImage *result,
+                                          NSDictionary *info) {
+        executeInMain(^{
+            resultHandler(result, info);
+        });
+    };
+    
+    PHImageRequestID requestID = [self.manager requestImageForAsset:asset
+                                                         targetSize:targetSize
+                                                        contentMode:self.contentMode
+                                                            options:self.options
+                                                      resultHandler:resultHandlerWrapper];
+    self.requests[identifier] = [NSNumber numberWithInteger:requestID];
 }
 
 - (void)cancelImageFetchingAtIndex:(NSInteger)index {
-    
+    NSNumber *identifier = [NSNumber numberWithInteger:index];
+    NSNumber *request = self.requests[identifier];
+    if (request != nil) {
+        [self.requests removeObjectForKey:identifier];
+        [self.manager cancelImageRequest:[request intValue]];
+    }
+}
+
+#pragma mark - requests
+
+- (NSMutableDictionary *)requests {
+    if (_requests)
+        return _requests;
+    _requests = [[NSMutableDictionary alloc] init];
+    return _requests;
+}
+
+#pragma mark - PHCachingImageManager
+
+- (PHImageManager *)manager {
+    if (_manager)
+        return _manager;
+    _manager = [PHImageManager defaultManager];
+    return _manager;
+}
+
+#pragma mark - Parameteres
+
+- (PHImageContentMode)contentMode {
+    return PHImageContentModeAspectFit;
+}
+
+- (PHImageRequestOptions *)options {
+    if (_options)
+        return _options;
+    _options = [[PHImageRequestOptions alloc] init];
+    _options.resizeMode = PHImageRequestOptionsResizeModeFast;
+    _options.deliveryMode = PHImageRequestOptionsDeliveryModeHighQualityFormat;
+    _options.networkAccessAllowed = YES;
+    return _options;
 }
 
 @end
